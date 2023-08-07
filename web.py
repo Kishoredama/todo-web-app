efd9b154cf6501807a21953bf2b9e08ae703faeb
Kishore Dama <kishoredama369@gmail.com>
2023-08-07 17:17:37 +0530

    Add function

--- web.py
+++ web.py
@@ -3,6 +3,12 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    print(todo)
+    functions.write_todos(todos)
+
 st.title("My ToDo App")
 st.subheader("This is my todo App")
 st.write("This App is to increase my productivity")
@@ -10,6 +16,9 @@ st.write("This App is to increase my productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='',placeholder='Add a new Todo')
+st.text_input(label="",placeholder='Add a new Todo...',
+              on_change=add_todo,key='new_todo')
 
 print("Hello")
+
+st.session_state
